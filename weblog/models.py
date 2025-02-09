4ba3b61ec4c074c8497e02c28714cdab9ecfdf70
mehrshad boleyla <boleyla.mehrshad1@gmail.com>
2025-02-09 23:16:30 +0330

    add view for blog details

--- weblog/models.py
+++ weblog/models.py
@@ -10,6 +10,7 @@ class Blog(models.Model):
     created_at = models.DateTimeField()
     content = models.TextField()
     timeread = models.CharField(max_length=200)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
